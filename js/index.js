f697cef23f9c6745c3a4b016d3ff2012f3621d6a
darkokitanoski-blip <darko.kitanoski@elev.ga.lbs.se>
2026-08-29 17:57:12 +0200

    detaljer, fix och kommentera

--- js/index.js
+++ js/index.js
@@ -6,56 +6,93 @@ const menubarburger = document.querySelector("#menubarburger")
 const selectedtabtext = document.querySelector(".menu-bar-wrapper-selected-tab")
 const userInfoButton = document.querySelector("#user-info-button")
 const wrapperInfo = document.querySelector(".wrapper-info")
+const menubarPath = menubarburger.querySelector("path")
+
+
+const burgerPath = "M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"
+const closePath = "M74 114L114 74L256 216L398 74L438 114L296 256L438 398L398 438L256 296L114 438L74 398L216 256Z"
+
 
 menubaricon.addEventListener("click", () => {
+
+    // stänga av menu-bar  
     if (menubar.classList.contains("active-menu-bar")) {
+
         menubar.classList.remove("active-menu-bar")
         hiddenul.classList.remove("visible-ul")
-        if (window.innerWidth >= 1000) {
+
+
+        setTimeout(() => {
+            menubarPath.setAttribute("d", burgerPath)
+
+        }, 150)
+
+
+        if (window.innerWidth >= 1300) {
 
             hero.style.left = "21%"
             menubaricon.querySelector("svg").style.left = "50%"
+
         } else {
+
             hero.style.left = "0%"
 
         }
+
         selectedtabtext.style.display = "block"
+
+    // öppna menu-bar 
     } else {
+
         menubar.classList.add("active-menu-bar")
-        if (window.innerWidth >= 1000) {
+
+        hiddenul.classList.add("visible-ul")
+
+        setTimeout(() => {
+            menubarPath.setAttribute("d", closePath)
+
+        }, 150)
+
+
+        if (window.innerWidth >= 1300) {
 
             hero.style.left = "8%"
             menubaricon.querySelector("svg").style.left = "20%"
+
         } else {
+
             hero.style.left = "0%"
+
         }
 
-        hiddenul.classList.add("visible-ul")
         selectedtabtext.style.display = "none"
 
     }
 })
 
-userInfoButton.addEventListener("click", () => {
-    wrapperInfo.classList.toggle("active-wrapper-info")
-})
 
 window.addEventListener("resize", () => {
 
     if (window.innerWidth >= 1300) {
+
         hero.style.left = "21%"
+
     } else {
+
         hero.style.left = "0%"
-        window.location.reload();
+        window.location.reload()
+
     }
 
     if (window.innerWidth >= 1000) {
         wrapperInfo.classList.remove("active-wrapper-info")
-
     }
+
 })
 
-
+userInfoButton.addEventListener("click", () => {
+    wrapperInfo.classList.toggle("active-wrapper-info")
+})
 
 
 const gamingIcon = document.getElementById("gaming");
